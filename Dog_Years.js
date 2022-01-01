3734640eab688a655fe0b70a8c8b78c4c8307c2b
Ascetic313 <iddrisualmuqadas74@gmail.com>
2022-01-01 17:34:46 +0000

    This is a project submission in dog years

---
+++ Dog_Years.js
@@ -0,0 +1,29 @@
+// Creating a variable named myAge//
+var myAge = 24
+
+// Creating a Variable named early years//
+var earlyYears = 2
+
+// Using the Multiplication operator//
+earlyYears = earlyYears *= 10.5
+
+// Using the substraction operator//
+var laterYears = myAge - 2
+
+//Using the Multiplication operator//
+laterYears = laterYears * 4
+
+// Testing codes//
+console.log(earlyYears)
+console.log(laterYears)
+
+// Using the Addition operator//
+var myAgeInDogYears = earlyYears + laterYears 
+
+// Converting name to lowercase//
+var myName = 'Alhassan'.toLowerCase()
+
+// Displaying a Message//
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
